85862b6d15b85d1170710b84775c07f0c68cc619
Jesse Medrano <Jtamedrano@gmail.com>
2020-12-17 21:18:07 -0800

    small syntax edits

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,28 +1,24 @@
-
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import PlantPage from "./components/PlantPage";
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import PlantPage from './components/PlantPage';
 
 import Home from './components/Home';
 import Login from './components/Login';
-import RegisterForm from "./components/RegisterForm";
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Switch>
+        <Switch>
           <PrivateRoute exact path="/plants" component={PlantPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/join" component={RegisterForm} />
           <Route exact path="/" component={Home} />
-          <Route path='/login' component={Login} />
-          <Route path='/join' component={RegisterForm} />
-          
         </Switch>
       </div>
     </Router>
-    
   );
 }
 
